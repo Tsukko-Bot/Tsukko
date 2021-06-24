dc2a6cff70153badcc0ccfb0bf9843521200f062
ByeMC <byemc@outlook.com>
2021-06-24 20:07:08 +0100

    added shit.

--- main.py
+++ main.py
@@ -16,14 +16,26 @@ except FileNotFoundError:
     input('Please put an API token in token.txt. (Press ENTER/RETURN to continue...) ')
     TOKEN = set_token()
 
-bot = commands.Bot(command_prefix='tsu/') # initilizes the bot, setting the prefix to "tsu/".
+# Change only the no_category default string
+help_command = commands.MinimalHelpCommand(
+    dm_help = True,
+    commands_heading = 'Tsukko/Help',
+    no_category = 'Commands'
+)
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title="Tsukko", description="ツッコ (Tsukko) is a Discord bot for managing your Discord server.", color=000000) #,color=Hex code
-    embed.add_field(name="Creator", value="[ByeMC#3276](https://byemc.xyz/)")
-    embed.add_field(name="Invite", value="To invite Tsukko to your Discord server, please go to the [GitHub repo](https://github.com/tsukko-bot/tsukko#invites)")
-    embed.set_image(url="https://static.byemc.xyz/tsukko/tsukko.png")
-    await ctx.send(embed=embed)
-    
+bot = commands.Bot(command_prefix = commands.when_mentioned_or('tsu/'), help_command = help_command) # initilizes the bot, setting the prefix to "tsu/".
+
+class misc():
+    @bot.command()
+    async def info(ctx):
+        '''Basic Bot Info.'''
+        embed = discord.Embed(title="Tsukko", description="ツッコ (Tsukko) is a Discord bot for managing your Discord server.", color=000000) #,color=Hex code
+        embed.add_field(name="Creator", value="[ByeMC#3276](https://byemc.xyz/)")
+        embed.add_field(name="Invite", value="To invite Tsukko to your Discord server, please go to the [GitHub repo](https://github.com/tsukko-bot/tsukko#invites)")
+        embed.set_image(url="https://static.byemc.xyz/tsukko/tsukko.png")
+        await ctx.send(embed=embed)
+
 bot.run(str(TOKEN))
+
+
+
